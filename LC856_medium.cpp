ebf55916cac81f87fb74ff85f8ad8f98035ce50c
Mrigank <mrigank432@gmail.com>
2022-03-17 15:30:25 +0530

    Create LC856_medium.cpp
    Common Stack problem

---
+++ LC856_medium.cpp
@@ -0,0 +1,23 @@
+
+
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        stack < int > st;
+        int score = 0;
+        for(auto x : s)
+        {
+            if(x == '(')
+            {
+                st.push(score);
+                score = 0;
+            }
+            else
+            {
+                score = st.top() + max(2 * score , 1);
+                st.pop();
+            }
+        }
+        return score;
+    }
+};
