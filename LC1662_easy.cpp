4329eaff1c3d37b45c3d3ab019b18d63422f3a86
Mrigank <mrigank432@gmail.com>
2022-03-22 21:12:44 +0530

    Create LC1662_easy.cpp

---
+++ LC1662_easy.cpp
@@ -0,0 +1,25 @@
+/*
+ Author: Mrigank
+Science is what we understand well enough to explain to a computer, Art is everything else we do - Donald E. Knuth
+Problem Link : https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+*/
+
+
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) 
+    {
+        string ans1 = "";
+        string ans2 = "";
+        
+        for(auto& word : word1)
+        {
+            ans1 += word;
+        }
+        for(auto& word : word2)
+        {
+            ans2 += word;
+        }
+        return (ans1 == ans2);
+    }
+};
